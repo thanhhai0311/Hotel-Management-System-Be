0d61ea3e6b7a4aa836e901a74ff2289d41c89791
Hai <nguyenthanhhai03112003@gmail.com>
2026-01-09 10:53:29 +0700

    fix-bug-time

--- src/main/java/com/javaweb/Application.java
+++ src/main/java/com/javaweb/Application.java
@@ -4,13 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class Application {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+        System.out.println("✅ Ứng dụng đã chuyển sang múi giờ: " + new java.util.Date());
+    }
 
 }
